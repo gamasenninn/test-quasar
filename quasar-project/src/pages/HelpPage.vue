5452e3036867086411806b90f6f9ae5a10a441da
gamasenninn <gamasenninn@yahoo.co.jp>
2022-10-09 19:02:41 +0900

    初回

---
+++ quasar-project/src/pages/HelpPage.vue
@@ -0,0 +1,17 @@
+<template>
+  <q-page class="q-pa-lg">
+    <h5 class="q-mt-none">Help</h5>
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ut quaerat libero tenetur eligendi nisi debitis assumenda quis! Voluptates, placeat dolores numquam ducimus ullam unde odit tempore fugiat ad omnis neque?</p>
+  </q-page>
+
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+
+export default defineComponent({
+  name: "HelpPage"
+})
+
+</script>
---
+++ quasar-project/src/pages/TodoItem.vue
@@ -0,0 +1,22 @@
+<template>
+  <q-page class="bg-grey-3 column">
+    <div><h5>Todo</h5></div>
+  </q-page>
+  <h5>Todo</h5>
+  <h5>Todo</h5>
+
+
+
+
+
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+
+export default defineComponent({
+  name: "TodoItem"
+})
+
+</script>
