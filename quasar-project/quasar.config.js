88e20f7819e2a7b7b4119c4518acb3e908443cf9
gamasenninn <gamasenninn@yahoo.co.jp>
2022-10-10 17:17:48 +0900

    完了

--- quasar-project/quasar.config.js
+++ quasar-project/quasar.config.js
@@ -104,7 +104,7 @@ module.exports = configure(function (ctx) {
       // directives: [],
 
       // Quasar plugins
-      plugins: []
+      plugins: ['Dialog','Notify']
     },
 
     // animations: 'all', // --- includes all animations
@@ -204,6 +204,7 @@ module.exports = configure(function (ctx) {
       bundler: 'packager', // 'packager' or 'builder'
 
       packager: {
+        platform: 'win32'
         // https://github.com/electron-userland/electron-packager/blob/master/docs/api.md#options
 
         // OS X / Mac App Store
